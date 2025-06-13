044c8357cae00d6f904206bf48d11c3a0ef0b41b
sharu2903 <sharu2903@gmail.com>
2025-06-13 12:34:21 +0530

    Configured database, hibernate using JPA and Entity Creation

--- reconciliation/src/main/java/com/identity/reconciliation/entity/ContactEntity.java
+++ reconciliation/src/main/java/com/identity/reconciliation/entity/ContactEntity.java
@@ -1,4 +1,39 @@
 package com.identity.reconciliation.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contact")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String phoneNumber;
+
+    private String email;
+
+    // refers to primary contact id
+    private Integer linkedId;
+
+    @Enumerated(EnumType.STRING)
+    private LinkPrecedence linkPrecedence;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    public enum LinkPrecedence {
+        PRIMARY,
+        SECONDARY
+    }
 }
